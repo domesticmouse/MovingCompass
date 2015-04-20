751f995d269a9539b28d185bc286a526517ce8cd
Brett Morgan <brettmorgan@google.com>
2015-04-20 16:03:08 +1000

    Initial code commit

---
+++ MovingCompass/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController, GMSMapViewDelegate {
+
+  @IBOutlet var mapView: GMSMapView!
+  @IBOutlet weak var label: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    mapView.delegate = self
+    mapView.settings.compassButton = true
+  }
+
+  // MARK - GMSMapViewDelegate
+  
+  func mapView(mapView: GMSMapView!, idleAtCameraPosition position: GMSCameraPosition!) {
+    label.text = "MapView idle"
+  }
+  
+  func mapView(mapView: GMSMapView!, willMove gesture: Bool) {
+    label.text = "MapView willMove"
+  }
+  
+  func mapView(mapView: GMSMapView!, didChangeCameraPosition position: GMSCameraPosition!) {
+    label.text = "MapView didChangeCameraPosition " +
+      "\(position.target.latitude),\(position.target.longitude) " +
+      "zoom: \(position.zoom) bearing: \(position.bearing)"
+  }
+}
+
